4f498136c7d2b419d4ef773fa17bacec012ed09d
Mangochicken <mangochkn@gmail.com>
2026-03-23 01:03:20 +0800

    Progress on Linger Keyword

--- ShadowfallCode/Cards/ShadowNecrobinder/ClenchFist.cs
+++ ShadowfallCode/Cards/ShadowNecrobinder/ClenchFist.cs
@@ -3,6 +3,7 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
 using Shadowfall.ShadowfallCode.Keywords;
 
@@ -10,16 +11,21 @@ namespace Shadowfall.ShadowfallCode.Cards.ShadowNecrobinder;
 
 public sealed class ClenchFist() : ShadowNecrobinderCard(1, CardType.Skill, CardRarity.Basic, TargetType.AnyEnemy)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
-    {
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
         new PowerVar<VulnerablePower>(2),
-    };
+    ];
 
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => new IHoverTip[]
-    {
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
         HoverTipFactory.FromPower<VulnerablePower>(),
         HoverTipFactory.FromKeyword(ShadowfallKeywords.Linger)
-    };
+    ];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords =>
+    [
+        ShadowfallKeywords.Linger
+    ];
 
     public override bool HasTurnEndInHandEffect => true;
 
@@ -35,13 +41,18 @@ public sealed class ClenchFist() : ShadowNecrobinderCard(1, CardType.Skill, Card
         DynamicVars.Vulnerable.UpgradeValueBy(1m);
     }
 
+    public override (PileType, CardPilePosition) ModifyCardPlayResultPileTypeAndPosition(CardModel card, bool isAutoPlay, ResourceInfo resources, PileType pileType, CardPilePosition position)
+    {
+        if (card.Owner != Owner) { return (pileType, position); }
+        if (pileType != PileType.Discard) { return (pileType, position); }
+        if (card.Keywords.Contains(ShadowfallKeywords.Linger)) { return (PileType.Draw, CardPilePosition.Random); }
+        return (pileType, position);
+    }
+
     public override async Task OnTurnEndInHand(PlayerChoiceContext choiceContext)
     {
-        // VfxCmd.PlayOnCreatureCenter(Owner.Creature, "vfx/vfx_power_up");
+        // ModifyCardPlayResultPileTypeAndPosition(this, false, new ResourceInfo{EnergySpent = 0, EnergyValue = 0, StarsSpent = 0, StarValue = 0}, PileType.Draw, CardPilePosition.Random);
         await PowerCmd.Apply<DrawCardsNextTurnPower>(Owner.Creature, 1m, Owner.Creature, this);
-        // GetResultPileType();
-        ModifyCardPlayResultPileTypeAndPosition(this, false, new ResourceInfo{EnergySpent = 0, EnergyValue = 0, StarsSpent = 0, StarValue = 0}, PileType.Draw, CardPilePosition.Random);
-        // await PowerCmd.Apply<Linger>(Owner.Creature, 1m, Owner.Creature)
-        await CardPileCmd.Add(this, PileType.Draw, CardPilePosition.Random);
+        // await CardPileCmd.Add(this, PileType.Draw, CardPilePosition.Random);
     }
 }
