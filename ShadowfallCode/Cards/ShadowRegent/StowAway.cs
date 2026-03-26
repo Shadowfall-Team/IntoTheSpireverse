21499af8759ead21d0254c7e62c3643acf2b69ee
Linniel Driver-Williams <LinnielDW@googlemail.com>
2026-03-26 21:02:30 +0100

    added StowAway

---
+++ ShadowfallCode/Cards/ShadowRegent/StowAway.cs
@@ -0,0 +1,45 @@
+﻿using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Shadowfall.ShadowfallCode.Powers.ShadowRegent;
+
+namespace Shadowfall.ShadowfallCode.Cards.ShadowRegent;
+
+public class StowAway() : ShadowRegentCard(1,
+    CardType.Attack,
+    CardRarity.Common,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(8, ValueProp.Move),
+        new CardsVar(1)
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        if (CombatState == null) return;
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .Targeting(play.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+
+        await CardSelectCmd.FromHand(choiceContext, Owner,
+            new CardSelectorPrefs(CargoSelectorPrefs.CargoSelectionPrompt, 1), null,
+            this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(1);
+        DynamicVars.Cards.UpgradeValueBy(1);
+    }
+}
