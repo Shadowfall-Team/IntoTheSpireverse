bc643f1098f03abecbac4754c38af7b837509ab9
nageld <nageld@bxscience.edu>
2026-03-26 01:13:44 -0400

    add block hovertip to share the load

--- ShadowfallCode/Cards/ShadowSilent/ShareTheLoad.cs
+++ ShadowfallCode/Cards/ShadowSilent/ShareTheLoad.cs
@@ -1,6 +1,7 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using Shadowfall.ShadowfallCode.Powers.ShadowSilent;
 
@@ -11,6 +12,11 @@ public sealed class ShareTheLoad() : ShadowSilentCard(2, CardType.Power, CardRar
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
         new PowerVar<ShareTheLoadPower>(1m),
+    ];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.Static(StaticHoverTip.Block)
     ];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
