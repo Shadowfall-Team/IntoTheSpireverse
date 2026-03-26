d1e2f89629b0c9e9ce149b426617b64b98af8a6e
nageld <nageld@bxscience.edu>
2026-03-26 00:55:50 -0400

    Share The Load

---
+++ ShadowfallCode/Cards/ShadowSilent/ShareTheLoad.cs
@@ -0,0 +1,25 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using Shadowfall.ShadowfallCode.Powers.ShadowSilent;
+
+namespace Shadowfall.ShadowfallCode.Cards.ShadowSilent;
+
+public sealed class ShareTheLoad() : ShadowSilentCard(2, CardType.Power, CardRarity.Rare, TargetType.None)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new PowerVar<ShareTheLoadPower>(1m),
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<ShareTheLoadPower>(Owner.Creature, DynamicVars[nameof(ShareTheLoadPower)].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
+}
