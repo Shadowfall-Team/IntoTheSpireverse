ced6d1e518ebf1cbd2d6c5c13e367df5c67279d0
Linniel Driver-Williams <LinnielDW@googlemail.com>
2026-03-28 21:03:33 +0100

    added ReinforcedBody

--- ShadowfallCode/Cards/ShadowRegent/ReinforcedBody.cs
+++ ShadowfallCode/Cards/ShadowRegent/ReinforcedBody.cs
@@ -1,23 +1,35 @@
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Shadowfall.ShadowfallCode.Cards.ShadowRegent;
 
-public class ReinforcedBody() : ShadowRegentCard(1,
-    CardType.Power,
+public class ReinforcedBody() : ShadowRegentCard(0,
+    CardType.Skill,
     CardRarity.Uncommon,
     TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+    protected override bool HasEnergyCostX => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlockVar(7, ValueProp.Move)
+    ];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
+        for (var i = 0; i < ResolveEnergyXValue(); i++)
+        {
+            await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, play);
+        }
     }
-    
+
     protected override void OnUpgrade()
     {
+        DynamicVars.Block.UpgradeValueBy(2);
     }
 }
