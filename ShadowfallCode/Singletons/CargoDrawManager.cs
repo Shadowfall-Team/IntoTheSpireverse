29d999932d9264ab6e4623eb309bfa1a91f352ba
Linniel Driver-Williams <LinnielDW@googlemail.com>
2026-03-31 00:26:07 +0200

    added CargoDrawManager singleton

---
+++ ShadowfallCode/Singletons/CargoDrawManager.cs
@@ -0,0 +1,26 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using Shadowfall.ShadowfallCode.CardPiles;
+
+namespace Shadowfall.ShadowfallCode.Singletons;
+
+public class CargoDrawManager : SingletonModel
+{
+    public override bool ShouldReceiveCombatHooks => true;
+
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        var cargoPile = CargoCardPile.CargoPileType.GetPile(player);
+        if (!cargoPile.IsEmpty)
+        {
+            var card = cargoPile.Cards.FirstOrDefault();
+            if (card != null)
+            {
+                await CardPileCmd.Add(card, PileType.Hand);
+            }
+        }
+    }
+}
