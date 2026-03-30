a45737adcde36b28843874e6a814831e60166327
Mangochicken <mangochkn@gmail.com>
2026-03-31 00:41:38 +0800

    [In-Progress] Add card frame shadow shader, implement on current characters

--- ShadowfallCode/Character/ShadowSilent/ShadowSIlentCardPool.cs
+++ ShadowfallCode/Character/ShadowSilent/ShadowSIlentCardPool.cs
@@ -11,6 +11,13 @@ public class ShadowSilentCardPool : CustomCardPoolModel
     public override string Title => "shadow_silent";
     public override string EnergyColorName => "silent";
 
+
+	public override string CardFrameMaterialPath => "shadow_silent";
+
+	public override Color DeckEntryCardColor => new("5EBD00");
+
+	public override Color EnergyOutlineColor => new("1A6625");
+
     /* These HSV values will determine the color of your card back.
     They are applied as a shader onto an already colored image,
     so it may take some experimentation to find a color you like.
@@ -18,17 +25,7 @@ public class ShadowSilentCardPool : CustomCardPoolModel
     public override float H => 1f; //Hue; changes the color.
     public override float S => 1f; //Saturation
     public override float V => 1f; //Brightness
-
-    //Alternatively, leave these values at 1 and provide a custom frame image.
-    /*public override Texture2D CustomFrame(CustomCardModel card)
-    {
-        //This will attempt to load Shadowfall/images/cards/frame.png
-        return PreloadManager.Cache.GetTexture2D("cards/frame.png".ImagePath());
-    }*/
-
-    //Color of small card icons
-    public override Color DeckEntryCardColor => new("ffffff");
-
+    
     public override bool IsColorless => false;
     
     protected override CardModel[] GenerateAllCards()
