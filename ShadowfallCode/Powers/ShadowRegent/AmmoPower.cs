01677c59b42bc5ee93ed6d36490db77c3ff80824
Linniel Driver-Williams <LinnielDW@googlemail.com>
2026-03-31 00:27:12 +0200

    changed ammo and volley to work like thorns, instead of an attack

--- ShadowfallCode/Powers/ShadowRegent/AmmoPower.cs
+++ ShadowfallCode/Powers/ShadowRegent/AmmoPower.cs
@@ -12,25 +12,32 @@ public class AmmoPower : CustomPowerModel
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
-    
+
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
         new DamageVar(8, ValueProp.Move),
     ];
-    
-    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+
+    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext,
+        CombatSide side)
     {
         if (side != CombatSide.Enemy)
             return;
 
-        var volleyDamage = DynamicVars.Damage.BaseValue + Owner.GetPowerAmount<VolleyDamageThisTurn>();
-        
-        await DamageCmd.Attack(volleyDamage)
-            .WithHitCount(Amount)
-            .TargetingRandomOpponents(CombatState)
-            // .WithHitFx("vfx/vfx_attack_slash", null, null)
-            .Execute(choiceContext);
-        
+        var volleyDamage = DynamicVars.Damage.BaseValue +
+                           Owner.GetPowerAmount<VolleyDamageThisTurn>();
+
+        for (var i = 0; i < Amount; i++)
+        {
+            var validTargets = CombatState.Enemies.Where(e => e.IsAlive);
+            var target = CombatState.RunState.Rng.CombatTargets.NextItem(validTargets);
+            if (target != null)
+            {
+                await CreatureCmd.Damage(choiceContext, target, volleyDamage,
+                    ValueProp.Unpowered, Owner);
+            }
+        }
+
         await PowerCmd.Remove<VolleyDamageThisTurn>(Owner);
         await PowerCmd.Remove(this);
     }
