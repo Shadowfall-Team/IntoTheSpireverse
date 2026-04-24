0d2850217bd3fa07bc2ce80b255339b388cb80ed
nageld <nageld@bxscience.edu>
2026-04-24 16:50:16 -0400

    remove unused imports

--- ShadowfallCode/Powers/SongOfSorrowPower.cs
+++ ShadowfallCode/Powers/SongOfSorrowPower.cs
@@ -1,15 +1,10 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using BaseLib.Abstracts;
-using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Models.Cards;
 using MegaCrit.Sts2.Core.ValueProps;
 using Void = MegaCrit.Sts2.Core.Models.Cards.Void;
 
