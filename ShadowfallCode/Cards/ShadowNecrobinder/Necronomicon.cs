cb271162cdfcc10db7fcb448e622486ba3269de1
Mangochicken <mangochkn@gmail.com>
2026-04-27 21:16:05 +0800

    Necronomicon implementation
    Relies on https://github.com/Alchyr/BaseLib-StS2/pull/79

---
+++ ShadowfallCode/Cards/ShadowNecrobinder/Necronomicon.cs
@@ -0,0 +1,27 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using Shadowfall.ShadowfallCode.Powers.ShadowNecrobinder;
+
+namespace Shadowfall.ShadowfallCode.Cards.ShadowNecrobinder;
+
+public sealed class Necronomicon() : ShadowNecrobinderCard(0, CardType.Power, CardRarity.Ancient, TargetType.Self)
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Eternal];
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new CardsVar(1)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        await PowerCmd.Apply<NecronomiconPower>(Owner.Creature, DynamicVars.Cards.BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Cards.UpgradeValueBy(1m);
+    }
+}
