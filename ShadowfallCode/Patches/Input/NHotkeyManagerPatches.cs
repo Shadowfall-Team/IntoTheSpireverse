e35da18079bde19d73775a467484d7978666a68c
LinnielDW <linnieldw@gmail.com>
2026-06-17 08:40:23 +0200

    added controller support for regent piles

---
+++ ShadowfallCode/Patches/Input/NHotkeyManagerPatches.cs
@@ -0,0 +1,72 @@
+﻿using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Context;
+using MegaCrit.Sts2.Core.ControllerInput;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+using MegaCrit.Sts2.Core.Nodes.CommonUi;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Runs;
+using Shadowfall.ShadowfallCode.ui;
+using GodotInput = Godot.Input;
+
+namespace Shadowfall.ShadowfallCode.Patches.Input;
+
+[HarmonyPatch(typeof(NHotkeyManager), nameof(NHotkeyManager._UnhandledInput))]
+public static class NHotkeyManagerPatches
+{
+    private static bool _comboConsumedLeftTrigger;
+    private static bool _comboConsumedRightTrigger;
+
+    [HarmonyPrefix]
+    public static bool UnhandledInputPrefix(InputEvent inputEvent)
+    {
+        if (_comboConsumedLeftTrigger && inputEvent.IsActionReleased(MegaInput.viewDrawPile, exactMatch: false))
+        {
+            _comboConsumedLeftTrigger = false;
+            return false;
+        }
+
+        if (_comboConsumedRightTrigger && inputEvent.IsActionReleased(MegaInput.viewDiscardPile, exactMatch: false))
+        {
+            _comboConsumedRightTrigger = false;
+            return false;
+        }
+
+        if (inputEvent.IsActionPressed(MegaInput.viewDiscardPile, exactMatch: false)
+            && GodotInput.IsActionPressed(MegaInput.viewDrawPile))
+        {
+            _comboConsumedLeftTrigger = true;
+            _comboConsumedRightTrigger = true;
+
+            var container = NCombatRoom.Instance?.Ui.GetNode<NCombatPilesContainer>("%CombatPileContainer");
+            var cargoPile = container?.GetNodeOrNull<NCargoPile>("_CargoPile");
+
+            if (cargoPile != null && cargoPile.IsVisibleInTree())
+            {
+                cargoPile.OnPressHandler();
+                cargoPile.OnReleaseHandler();
+            }
+
+            return false;
+        }
+
+        if (inputEvent.IsActionPressed(MegaInput.select, exactMatch: false)
+            && GodotInput.IsActionPressed(Controller.leftTrigger))
+        {
+            _comboConsumedLeftTrigger = true;
+
+            var localCreature = LocalContext.GetMe(RunManager.Instance.State.Players).Creature;
+            var ammoButton = NCombatRoom.Instance?.GetCreatureNode(localCreature)
+                ?.GetNodeOrNull<NAmmoButton>("AmmoButton");
+            if (ammoButton != null && ammoButton.IsVisibleInTree())
+            {
+                ammoButton.OnPressHandler();
+                ammoButton.OnReleaseHandler();
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+}
