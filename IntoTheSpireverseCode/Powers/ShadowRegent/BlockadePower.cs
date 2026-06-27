34f3537f780b37e7473d764f2d6b2f2db0401476
LinnielDW <linnieldw@gmail.com>
2026-06-27 22:04:09 +0200

    fixed blockade power parent

--- IntoTheSpireverseCode/Powers/ShadowRegent/BlockadePower.cs
+++ IntoTheSpireverseCode/Powers/ShadowRegent/BlockadePower.cs
@@ -1,12 +1,11 @@
-﻿using BaseLib.Abstracts;
-using IntoTheSpireverse.IntoTheSpireverseCode.Ammo;
+﻿using IntoTheSpireverse.IntoTheSpireverseCode.Ammo;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace IntoTheSpireverse.IntoTheSpireverseCode.Powers.ShadowRegent;
 
-public class BlockadePower : CustomPowerModel, IAmmoLoadedListener
+public class BlockadePower : ShadowPowerModel, IAmmoLoadedListener
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
