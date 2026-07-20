f5ad56d3d42b13aaafd942bc3d7b093de8178779
Luna Astra Cassiopeia <kadabe380@gmail.com>
2026-07-20 20:23:11 +0800

    all character art implemented!

--- IntoTheSpireverseCode/Character/ShadowSilent/ShadowSilent.cs
+++ IntoTheSpireverseCode/Character/ShadowSilent/ShadowSilent.cs
@@ -49,6 +49,17 @@ public class ShadowSilent : PlaceholderCharacterModel, IAltCharacter, IIntoTheSp
     public override CardPoolModel CardPool => ModelDb.CardPool<ShadowSilentCardPool>();
     public override RelicPoolModel RelicPool => ModelDb.RelicPool<ShadowSilentRelicPool>();
     public override PotionPoolModel PotionPool => ModelDb.PotionPool<ShadowSilentPotionPool>();
+    
+    
+    public override string CustomArmPointingTexturePath => "multiplayer_hand_silent_point.png".ShadowSilentPath();
+    public override string CustomArmRockTexturePath => "multiplayer_hand_silent_rock.png".ShadowSilentPath();
+    public override string CustomArmPaperTexturePath => "multiplayer_hand_silent_paper.png".ShadowSilentPath();
+    public override string CustomArmScissorsTexturePath => "multiplayer_hand_silent_scissors.png".ShadowSilentPath();
+    
+    public override string CustomIconTexturePath => "character_icon_silent.png".ShadowSilentPath();
+    public override string? CustomIconOutlineTexturePath => "character_icon_silent_outline.png".ShadowSilentPath();
+    public override string CustomCharacterSelectIconPath => "char_select_silent.png".ShadowSilentPath();
+    public override string CustomMapMarkerPath => "map_marker_silent.png".ShadowSilentPath();
 
     /*  PlaceholderCharacterModel will utilize placeholder basegame assets for most of your character assets until you
         override all the other methods that define those assets.
@@ -61,4 +72,6 @@ public class ShadowSilent : PlaceholderCharacterModel, IAltCharacter, IIntoTheSp
     
     public override string CustomVisualPath => "res://IntoTheSpireverse/scenes/creature_visuals/shadowsilent.tscn";
     public override string CustomMerchantAnimPath => "res://IntoTheSpireverse/scenes/merchant/shadowsilent_merchant.tscn";
+    public override string CustomCharacterSelectBg => "res://IntoTheSpireverse/scenes/screens/char_select/shadowsilent.tscn";
+    public override string CustomRestSiteAnimPath => "res://IntoTheSpireverse/scenes/rest_site/shadowsilent_rest_site.tscn";
 }
