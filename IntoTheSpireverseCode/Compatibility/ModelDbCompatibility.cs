0e8de670e81ce41d836ebc36e78aee275250e30e
Mangochicken <mangochkn@gmail.com>
2026-07-21 01:04:31 +0800

    drop unused single card method

--- IntoTheSpireverseCode/Compatibility/ModelDbCompatibility.cs
+++ IntoTheSpireverseCode/Compatibility/ModelDbCompatibility.cs
@@ -5,16 +5,6 @@ namespace IntoTheSpireverse.IntoTheSpireverseCode.Compatibility;
 
 public static class ModelDbCompatibility
 {
-    // safely try to get the ModelDb instance of the
-    internal static CardModel? GetCardModelSafely(Type? cardType)
-    {
-        if (cardType is null) { return null; }
-        var cardMethod = AccessTools.Method(
-                typeof(ModelDb), nameof(ModelDb.Card)
-            ).MakeGenericMethod([cardType]);
-        return (CardModel?) cardMethod.Invoke(null, null);
-    }
-
     internal static CardModel[] GetCardModelsSafely(Type?[] cardTypes)
     {
         CardModel[] cards = [];
