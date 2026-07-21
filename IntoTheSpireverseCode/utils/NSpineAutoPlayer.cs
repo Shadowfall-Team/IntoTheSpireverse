8ce922a3500ebb55e5d165daff0c1e5c8ad46ddc
Luna Astra Cassiopeia <kadabe380@gmail.com>
2026-07-21 17:15:28 +0800

    Animations for Captain and Tectonic

---
+++ IntoTheSpireverseCode/utils/NSpineAutoPlayer.cs
@@ -0,0 +1,24 @@
+﻿using Godot;
+using MegaCrit.Sts2.Core.Bindings.MegaSpine;
+using MegaCrit.Sts2.Core.Helpers;
+
+namespace IntoTheSpireverse.IntoTheSpireverseCode.utils;
+
+public partial class NSpineAutoPlayer : Node
+{
+    public override void _Ready()
+    {
+        MegaSprite sprite = new MegaSprite((Variant) (GodotObject) this.GetParent());
+        this.RunWhenSpineReady(sprite, (Action<MegaAnimationState>) (animState =>
+        {
+            IReadOnlyList<string> animationNames = sprite.GetSkeleton().GetData().GetAnimationNames();
+            if (animationNames.Count != 1)
+                throw new InvalidOperationException($"{nameof (NSpineAutoPlayer)}'s parent's skeleton data must have exactly 1 animation. This has {animationNames.Count}.");
+            animState.SetAnimation(animationNames[0]);
+        }));
+    }
+  
+    public override void _Process(double delta)
+    {
+    }
+}
