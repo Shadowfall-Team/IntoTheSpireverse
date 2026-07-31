0478fcbdab9a478e38ec1731a9d6521b726bc7c5
LinnielDW <linnieldw@gmail.com>
2026-07-31 20:44:07 +0200

    updated spireverse to v110

--- IntoTheSpireverseCode/Ui/ComboControllerIcons.cs
+++ IntoTheSpireverseCode/Ui/ComboControllerIcons.cs
@@ -1,3 +1,4 @@
+using BaseLib.Utils;
 using Godot;
 using MegaCrit.Sts2.Core.ControllerInput;
 using MegaCrit.Sts2.Core.Nodes.CommonUi;
@@ -27,7 +28,8 @@ public class ComboControllerIcons
         var im = NInputManager.Instance;
         if (cm == null || im == null) return;
 
-        var show = cm.IsUsingController && enabled;
+        //TODO: migrate this whole thing to use NHotkeyIcons once v110 is released to main
+        var show = cm.IsUsingButtonInputsCompatibility() && enabled;
         _iconA.Visible = show;
         _iconB.Visible = show;
         _separator?.Visible = show;
