3b89d9b549ec6020040fc5c1d44773dc225294f7
Indi Esneault <indiesneault@gmail.com>
2026-08-19 21:41:00 -0700

    parallel stone compat

--- IntoTheSpireverseCode/Relics/ParallelStone.cs
+++ IntoTheSpireverseCode/Relics/ParallelStone.cs
@@ -5,6 +5,7 @@ using Godot;
 using IntoTheSpireverse.IntoTheSpireverseCode.Extensions;
 using IntoTheSpireverse.IntoTheSpireverseCode.Utils;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Debug;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.Models;
@@ -73,15 +74,45 @@ public class ParallelStone : CustomRelicModel
         if (!options.Flags.HasFlag(CardCreationFlags.IsCardReward)) return options;
         if (options.CardPools.All(p => p.IsColorless)) return options;
 
+        bool onBeta = int.Parse((ReleaseInfoManager.Instance.ReleaseInfo?.Version ?? "v0.0.0").Split('.')[1]) > 107;
+
         var mirrorPools = AltCharacterUtil.GetMirrorCharacters(player.Character).Select(c => c.CardPool);
-        options = options.WithCardPools(options.CardPools.Union(mirrorPools));
 
         // The mirror pool contains duplicates of cards we already have, which would otherwise show up twice and roll
         // at double odds. WithFilter overwrites, so keep whatever filter was already there.
         var bannedIds = AltCharacterUtil.GetBannedCardIds(player.Character);
-        if (bannedIds.Count == 0) return options;
 
-        var existingFilter = options.CardPoolFilter;
-        return options.WithFilter(c => !bannedIds.Contains(c.Id) && (existingFilter == null || existingFilter(c)));
+        if (onBeta) {
+            var WithPoolsMethod = options.GetType().GetMethod("WithCardPools", [typeof(IEnumerable<CardPoolModel>)]);
+            var WithFilterMethod = options.GetType().GetMethod("WithFilter", [typeof(Func<CardModel, bool>)]);
+
+            if (WithPoolsMethod != null) {
+                options = (CardCreationOptions)WithPoolsMethod.Invoke(options, [options.CardPools.Union(mirrorPools)])!;
+            }
+
+            if (bannedIds.Count > 0 && WithFilterMethod != null) {
+                var existingFilter = options.CardPoolFilter;
+                Func<CardModel, bool> newFilter = c => !bannedIds.Contains(c.Id) && (existingFilter == null || existingFilter(c));
+                options = (CardCreationOptions)WithFilterMethod.Invoke(options, [newFilter])!;
+            }
+        } else {
+            var cardPool = options.CardPools.Union(mirrorPools).SelectMany(pool =>
+                    pool.GetUnlockedCards(player.UnlockState, player.RunState.CardMultiplayerConstraint))
+                    .Where(c => !bannedIds.Contains(c.Id))
+                    .ToArray();
+
+            if (cardPool.Length == 0) {
+                cardPool = player.Character.CardPool.GetUnlockedCards(player.UnlockState, player.RunState.CardMultiplayerConstraint)
+                    .Where(c => !bannedIds.Contains(c.Id)).ToArray();
+            }
+
+            var WithCustomPoolMethod = options.GetType().GetMethod("WithCustomPool", [typeof(IEnumerable<CardModel>), typeof(CardRarityOddsType?)]);
+            if (WithCustomPoolMethod != null)
+            {
+                options = (CardCreationOptions)WithCustomPoolMethod.Invoke(options, [cardPool, null])!;
+            }
+        }
+
+        return options;
     }
 }
