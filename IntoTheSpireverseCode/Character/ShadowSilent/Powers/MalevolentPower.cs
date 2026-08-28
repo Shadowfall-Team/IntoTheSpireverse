f82717f7770fe5dee96c7f1d283248b37fb69f65
Luna Astra Cassiopeia <kadabe380@gmail.com>
2026-08-28 16:48:05 +0800

    Malevolent

---
+++ IntoTheSpireverseCode/Character/ShadowSilent/Powers/MalevolentPower.cs
@@ -0,0 +1,32 @@
+﻿
+using Godot;
+using IntoTheSpireverse.IntoTheSpireverseCode.Keywords;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+
+namespace IntoTheSpireverse.IntoTheSpireverseCode.Character.ShadowSilent.Powers;
+
+public class MalevolentPower : ShadowPowerModel, IntoTheSpireverseKeywords.IDeviousDiscardListener
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromKeyword(IntoTheSpireverseKeywords.Muddle),
+    ];
+
+    public int ModifyDeviousDiscard(int originalAmount)
+    {
+        return originalAmount + Amount;
+    }
+}
--- IntoTheSpireverseCode/Character/ShadowSilent/Powers/SpearheadPower.cs
+++ IntoTheSpireverseCode/Character/ShadowSilent/Powers/SpearheadPower.cs
@@ -2,12 +2,13 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Models;
 
 namespace IntoTheSpireverse.IntoTheSpireverseCode.Character.ShadowSilent.Powers;
 
-public class SpearheadPower : ShadowPowerModel
+public class SpearheadPower : ShadowPowerModel, IntoTheSpireverseKeywords.IModifyDeviousListener
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
@@ -22,7 +23,7 @@ public class SpearheadPower : ShadowPowerModel
         return Owner != card.Owner.Creature ? originalValue : originalValue + Amount;
     }
 
-    public override async Task BeforeCardPlayed(CardPlay cardPlay)
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext,CardPlay cardPlay)
     {
         if (cardPlay.Card.Owner.Creature != Owner || !cardPlay.Card.Keywords.Contains(IntoTheSpireverseKeywords.Devious))
             return;
