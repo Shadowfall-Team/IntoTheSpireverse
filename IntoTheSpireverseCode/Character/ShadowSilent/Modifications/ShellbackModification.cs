c24a9e1f9ef85a330fa82b791436d9ca1ca36600
Luna Astra Cassiopeia <kadabe380@gmail.com>
2026-09-01 14:01:19 +0800

    shellback now modifies

---
+++ IntoTheSpireverseCode/Character/ShadowSilent/Modifications/ShellbackModification.cs
@@ -0,0 +1,27 @@
+﻿using IntoTheSpireverse.IntoTheSpireverseCode.Character.ShadowSilent.Cards;
+using IntoTheSpireverse.IntoTheSpireverseCode.Keywords;
+using IntoTheSpireverse.IntoTheSpireverseCode.Modifications;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace IntoTheSpireverse.IntoTheSpireverseCode.Character.ShadowSilent.Modifications;
+
+public sealed class ShellbackModification : Modification
+{
+    protected override bool AppendsTextToCardDescription => false;
+
+    protected override ModelId SourceCardId => ModelDb.Card<Shellback>().Id;
+    
+    public override Decimal ModifyBlockAdditive(
+        Creature target,
+        Decimal block,
+        ValueProp props,
+        CardModel? cardSource,
+        CardPlay? cardPlay) =>
+        cardSource == Owner ? Amount : 0m;
+
+}
