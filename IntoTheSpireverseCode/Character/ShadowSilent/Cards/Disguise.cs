388ea5bc2b7cd93d3274e22cd2fb238573352c1c
Mangochicken <mangochkn@gmail.com>
2026-09-11 23:46:07 +0800

    revert disguise while it's broken

--- IntoTheSpireverseCode/Character/ShadowSilent/Cards/Disguise.cs
+++ IntoTheSpireverseCode/Character/ShadowSilent/Cards/Disguise.cs
@@ -1,14 +1,16 @@
-﻿using IntoTheSpireverse.IntoTheSpireverseCode.Character.ShadowSilent.Cards.Colorless;
-using MegaCrit.Sts2.Core.Animation;
+﻿using MegaCrit.Sts2.Core.Animation;
+using IntoTheSpireverse.IntoTheSpireverseCode.Character.ShadowSilent.Cards.Colorless;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 
 namespace IntoTheSpireverse.IntoTheSpireverseCode.Character.ShadowSilent.Cards;
 
 public sealed class Disguise() : ShadowSilentCard(-1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
 {
+
     protected override bool HasEnergyCostX => true;
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
@@ -25,6 +27,15 @@ public sealed class Disguise() : ShadowSilentCard(-1, CardType.Skill, CardRarity
             .Select(_ => CombatState.CreateCard<Scale>(Owner));
 
         await CardPileCmd.AddGeneratedCardsToCombat(scales, PileType.Hand, Owner);
+    }
+
+    public override async Task AfterCardDiscarded(
+        PlayerChoiceContext choiceContext,
+        CardModel card)
+    {
+        if (card != this || CombatState == null)
+            return;
+        await CardPileCmd.Add(this, PileType.Draw, CardPilePosition.Top);
     }
 
     protected override void OnUpgrade()
