7a15bc7f61d3730ee31c05d7a460d401ae18fbe3
LinnielDW <linnieldw@gmail.com>
2026-09-11 19:20:52 +0200

    fixed missing EnergyColorName for ShadowRegentRelicPool

--- IntoTheSpireverseCode/Character/ShadowRegent/ShadowRegentRelicPool.cs
+++ IntoTheSpireverseCode/Character/ShadowRegent/ShadowRegentRelicPool.cs
@@ -7,7 +7,7 @@ namespace IntoTheSpireverse.IntoTheSpireverseCode.Character.ShadowRegent;
 
 public class ShadowRegentRelicPool : CustomRelicPoolModel
 {
-    public override string EnergyColorName => ShadowRegent.CharacterId;
+    public override string EnergyColorName => "regent";
     public override Color LabOutlineColor => ShadowRegent.Color;
 
     protected override IEnumerable<RelicModel> GenerateAllRelics()
